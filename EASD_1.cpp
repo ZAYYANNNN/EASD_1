7f412c4569782b488d496ecc2c8a6657667c284f
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-04-07 10:58:11 +0700

    membuat mov dan bilangan

--- EASD_1.cpp
+++ EASD_1.cpp
@@ -1,8 +1,8 @@
-// karena algoritma adalah serangkaian langkah-langkah terdefinisi dengan jelas dan terstruktur yang dirancang untuk menyelesaikan masalah tertentu
-//
+//karena algoritma bisa mempermudah
+// static dan dynamic
 
-
-
+// selection sort
+// quadratic = bubblesort,selectionsort,insertionsort - long linear= quick sort, merge sort
 
 
 #include <iostream>
@@ -31,5 +31,45 @@ void input() {
 	cout << "\nMasukan elemnt array" << endl;
 	cout << "============================" << endl;
 
-	for (int i = )
+	for (int i = 0;i < n; i++) {
+		cout << (i + 1) << "). ";
+		cin >> zayyan[i];
+	}
+}
+
+void swap(int x, int y) {
+	int temp;
+	temp = zayyan[x];
+	zayyan[x] = zayyan[y];
+	zayyan[y] = temp;
+}
+void Algorithm(int low, int high) {
+	int pivot, i, zyn;
+	if (low > high)
+		return;
+
+	pivot = zayyan[low];
+	i = low + 1;
+	zyn = high;
+
+	while (i <= zyn); {
+		while ((zayyan[i] <= pivot) && (i <= high)) {
+			i++;
+			cmp_count++;
+		}
+		cmp_count++;
+		while ((zayyan[zyn] > pivot) && (zyn >= low)) {
+			zyn--;
+			cmp_count++;
+		}
+	}
+	cmp_count++;
+	if (i < zyn) {
+		swap(i, zyn);
+		mov_count++;
+	}
+	if (low < zyn) {
+		swap(low, zyn)
+			mov_count++;
+	}
 }
