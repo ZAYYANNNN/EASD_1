dc7ee95ccbe39e4656e5e5201a655eaae5592d42
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-04-07 10:40:30 +0700

    Add project files.

---
+++ EASD_1.cpp
@@ -0,0 +1,35 @@
+// karena algoritma adalah serangkaian langkah-langkah terdefinisi dengan jelas dan terstruktur yang dirancang untuk menyelesaikan masalah tertentu
+//
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int zayyan[95];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukan Panjang Element Array : ";
+		cin >> n;
+
+		if (n <= 95)
+			break;
+		else
+			cout << "\n--------------------";
+		cout << "\nMAKSIMUM PANJANG ARRAY ADALAH 95";
+		cout << "\n-------------------------" << endl;
+		cout << endl;
+	}
+
+	cout << "============================" << endl;
+	cout << "\nMasukan elemnt array" << endl;
+	cout << "============================" << endl;
+
+	for (int i = )
+}
